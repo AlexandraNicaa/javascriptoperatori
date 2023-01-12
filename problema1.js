fb329d0ae6c4e0cb528a152a781c31ca87aa0c46
Ale Nica <alexandrahetea99@gmail.com>
2023-01-12 13:30:12 +0200

    added homework

---
+++ problema1.js
@@ -0,0 +1,14 @@
+const rezultat1 = 2 + 3 * 4 - 5
+console.log("Rezultat1: ", rezultat1);
+
+const rezultat2 = 4 * (2 + 6 / 3)
+console.log("Rezultat2: ", rezultat2);
+
+const rezultat3 = 2 * 3**2 +  2**5
+console.log("Rezultat3: ", rezultat3);
+
+const rezultat4 = (5 * (3 + 2/2)) +3
+console.log("Rezultat4: ", rezultat4);
+
+const rezultat5 = (2**5 - 5**2 + 3 * (5 - 4/2)) / (2**5 + 5**2)
+console.log("Rezultat5: ", rezultat5);
---
+++ problema2.js
@@ -0,0 +1,8 @@
+let x = 5
+let y = 8
+
+const rez1 = 2 * x - 5 * y;
+console.log("Rezultat 1:", rez1)
+
+const rez2 = 3 * x + 9 * y;
+console.log("Rezultat 2:", rez2)
